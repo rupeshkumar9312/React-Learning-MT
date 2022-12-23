8fd4306f7bba2566f61dd72406e7e5445a7a658b
rupeshkumar <rupeshkumar9312@gmail.com>
2022-12-23 14:05:24 +0530

    07-addding-normal-javascript-logic

--- react-complete-guide/src/components/ExpenseItem.jsx
+++ react-complete-guide/src/components/ExpenseItem.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
-    const expenseDate = props.date.toLocaleString();
+    const month = props.date.toLocaleString('en-US', { month: 'long' })
+    const year = props.date.getFullYear();
+    const date = props.date.toLocaleString('en-US', { day: '2-digit' })
     const title = props.title
     const amount = props.amount
     return (
         <div className='expense-item'>
-            <div>{expenseDate}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{date}</div>
+            </div>
             <div className='expense-item__description'>
                 <h2>{title}</h2>
                 <div className='expense-item__price'>${amount}</div>
