03d00dea397eb05695e0f74f3f475c7c7e313622
rupeshkumar <rupeshkumar9312@gmail.com>
2022-12-23 12:59:23 +0530

    04-add-css

--- react-complete-guide/src/components/ExpenseItem.jsx
+++ react-complete-guide/src/components/ExpenseItem.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import './ExpenseItem.css'
 
 function ExpenseItem() {
   return (
-    <div>
+    <div className='expense-item'>
         <div>December 23, 2022</div>
-        <div>
+        <div className='expense-item__description'>
             <h2>Car Insurance</h2>
-            <div>$296.23</div>
+            <div className='expense-item__price'>$296.23</div>
         </div>
     </div>
   )
