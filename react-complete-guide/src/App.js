d6fb1f3717145dcdd83ef289e4eb6d0238bb98df
rupeshkumar <rupeshkumar9312@gmail.com>
2022-12-23 10:48:51 +0530

    ExpenseItem added

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Let's Get Started</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-complete-guide/src/components/ExpenseItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ExpenseItem() {
+  return (
+    <div>
+        <h2>ExpenseItem</h2>
+    </div>
+  )
+}
+
+export default ExpenseItem
