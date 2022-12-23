d9adad491b5d7ac5eac291ffe49aeb925238efb6
rupeshkumar <rupeshkumar9312@gmail.com>
2022-12-23 13:44:00 +0530

    05-outputting-dynamic-data

--- react-complete-guide/src/components/ExpenseItem.jsx
+++ react-complete-guide/src/components/ExpenseItem.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import './ExpenseItem.css'
 
 function ExpenseItem() {
-  return (
-    <div className='expense-item'>
-        <div>December 23, 2022</div>
-        <div className='expense-item__description'>
-            <h2>Car Insurance</h2>
-            <div className='expense-item__price'>$296.23</div>
+    const expenseDate = new Date(2021, 2, 28);
+    const title = 'Car Insurance'
+    const amount = 296.23
+    return (
+        <div className='expense-item'>
+            <div>{expenseDate}</div>
+            <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>${amount}</div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ExpenseItem
